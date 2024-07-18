720e483770913ad15dce272298ee36d3be01d6a9
Florin Pinta <xttrust@live.com>
2024-07-18 14:44:18 +0000

    Prepare the files for the first deployment to Heroku

--- bookings/settings.py
+++ bookings/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-af2jn@ftza8g+@nyk80u59!qvrszm0$ftd*ywv9$ihrh^li^@q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [".codeinstitute-ide.net"]
+ALLOWED_HOSTS = [".codeinstitute-ide.net", ".herokuapp.com"]
 
 
 # Application definition
