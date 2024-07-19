e6f90cf79efc4a9c26f854f6bea686f29b28e710
Florin Pinta <xttrust@live.com>
2024-07-19 20:33:39 +0000

    Added django urils, format_html to format HTML

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import MenuCategory, MenuItem
 from django_summernote.admin import SummernoteModelAdmin
+from django.utils.html import format_html
 
 @admin.register(MenuCategory)
 class MenuCategoryAdmin(admin.ModelAdmin):
@@ -9,7 +10,11 @@ class MenuCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(MenuItem)
 class MenuItemAdmin(SummernoteModelAdmin):
-    list_display = ('name', 'category', 'description', 'price')
+    list_display = ('name', 'category', 'formatted_description', 'price')
     list_filter = ('category',)
     search_fields = ('name', 'description')
     summernote_fields = ('description',)
+    
+    def formatted_description(self, obj):
+        return format_html(obj.description)
+    formatted_description.short_description = 'Description'
