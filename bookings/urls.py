03af3544eb6b5cc2541f2e52126637a06510f659
Florin Pinta <xttrust@live.com>
2024-07-19 23:16:28 +0000

    Added Django allauth for account management

--- bookings/urls.py
+++ bookings/urls.py
@@ -21,8 +21,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
     path('tables/', include('table.urls')),
     path('menu/', include('menu.urls')),
+    
     path('', include('pages.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
