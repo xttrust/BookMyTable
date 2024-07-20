7f00a441dcc906e9016857082fe2406fe9f0b16e
Florin Pinta <xttrust@live.com>
2024-07-20 20:31:06 +0000

    Limit the number of menu items for homepage

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
 from menu.models import MenuCategory, MenuItem
 
+from django.views.generic import TemplateView
+from menu.models import MenuCategory, MenuItem
+
 class HomePageView(TemplateView):
     """
     View for the home page. Inherits from TemplateView.
@@ -18,7 +21,8 @@ class HomePageView(TemplateView):
             **kwargs: Additional keyword arguments passed to the method.
 
         Returns:
-            dict: Context data including menu items categorized by their category.
+            dict: Context data including menu items categorized by their category,
+                  limited to 9 items per category.
         """
         # Retrieve the base context data from the parent class
         context = super().get_context_data(**kwargs)
@@ -27,12 +31,17 @@ class HomePageView(TemplateView):
         categories = MenuCategory.objects.all()
         
         # Create a list of tuples where each tuple contains a category and its corresponding menu items
-        menu_items_by_category = [(category, MenuItem.objects.filter(category=category)) for category in categories]
+        # Limit the number of menu items per category to 9
+        menu_items_by_category = [
+            (category, MenuItem.objects.filter(category=category)[:9])  # Limit to 9 items
+            for category in categories
+        ]
         
         # Add the categorized menu items to the context data
         context['menu_items_by_category'] = menu_items_by_category
         
         return context
+
 
 class AboutPageView(TemplateView):
     """
