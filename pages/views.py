f1c361e0a3bb706359f8d95290fde20debcfd584
Florin Pinta <xttrust@live.com>
2024-07-20 19:43:36 +0000

    Added comments to pages views.py

--- pages/views.py
+++ pages/views.py
@@ -2,24 +2,52 @@ from django.views.generic import TemplateView
 from menu.models import MenuCategory, MenuItem
 
 class HomePageView(TemplateView):
+    """
+    View for the home page. Inherits from TemplateView.
+
+    Attributes:
+        template_name (str): The template to be used for rendering the home page.
+    """
     template_name = 'pages/home.html'
     
     def get_context_data(self, **kwargs):
+        """
+        Returns the context data for rendering the home page.
+
+        Args:
+            **kwargs: Additional keyword arguments passed to the method.
+
+        Returns:
+            dict: Context data including menu items categorized by their category.
+        """
+        # Retrieve the base context data from the parent class
         context = super().get_context_data(**kwargs)
         
-        # Fetch all categories
+        # Fetch all menu categories from the database
         categories = MenuCategory.objects.all()
         
-        # Create a list of tuples (category, menu_items)
+        # Create a list of tuples where each tuple contains a category and its corresponding menu items
         menu_items_by_category = [(category, MenuItem.objects.filter(category=category)) for category in categories]
         
+        # Add the categorized menu items to the context data
         context['menu_items_by_category'] = menu_items_by_category
         
         return context
 
 class AboutPageView(TemplateView):
+    """
+    View for the about page. Inherits from TemplateView.
+
+    Attributes:
+        template_name (str): The template to be used for rendering the about page.
+    """
     template_name = 'pages/about.html'
 
 class MenuPageView(TemplateView):
-    template_name = 'pages/menu.html'
+    """
+    View for the menu page. Inherits from TemplateView.
 
+    Attributes:
+        template_name (str): The template to be used for rendering the menu page.
+    """
+    template_name = 'pages/menu.html'
