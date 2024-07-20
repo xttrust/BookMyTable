a4d913ff70f48349df5c9abb76eb90fadf700326
Florin Pinta <xttrust@live.com>
2024-07-20 01:38:55 +0000

    Made our menu from homepage to be dinamyc

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,21 @@
 from django.views.generic import TemplateView
+from menu.models import MenuCategory, MenuItem
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Fetch all categories
+        categories = MenuCategory.objects.all()
+        
+        # Create a list of tuples (category, menu_items)
+        menu_items_by_category = [(category, MenuItem.objects.filter(category=category)) for category in categories]
+        
+        context['menu_items_by_category'] = menu_items_by_category
+        
+        return context
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
