9053d2c59e53d63a4ee9e349e47c478e8cee5afd
Florin Pinta <xttrust@live.com>
2024-07-20 21:20:02 +0000

    Implement paginated menu items with tabbed display

--- menu/views.py
+++ menu/views.py
@@ -1,22 +1,37 @@
-from django.views.generic import ListView
+from django.core.paginator import Paginator
+from django.utils.text import slugify
+from django.views.generic import TemplateView
 from .models import MenuCategory, MenuItem
 
-class MenuItemListView(ListView):
+class MenuItemListView(TemplateView):
     """
-    View for listing all menu items.
+    View for listing menu items with pagination and tabbed display.
 
     Attributes:
-        model (Model): The model to use for retrieving menu items.
         template_name (str): The template to be used for rendering the menu items.
-        context_object_name (str): The name of the context variable to be used in the template.
     """
     template_name = 'menu/menu_item_list.html'
-    context_object_name = 'menu_items_by_category'
 
-    def get_queryset(self):
+    def get_context_data(self, **kwargs):
+        """
+        Returns the context data for rendering the menu items.
+
+        Args:
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            dict: Context data including paginated menu items categorized by their category.
+        """
+        context = super().get_context_data(**kwargs)
         categories = MenuCategory.objects.all()
-        menu_items_by_category = [
-            (category, MenuItem.objects.filter(category=category)[:9])  # Limit to 9 items
-            for category in categories
-        ]
-        return menu_items_by_category
+
+        menu_items_by_category = []
+        for category in categories:
+            items = MenuItem.objects.filter(category=category)
+            paginator = Paginator(items, 1)  # 9 items per page
+            page_number = self.request.GET.get(f'{slugify(category.name)}_page', 1)
+            page_obj = paginator.get_page(page_number)
+            menu_items_by_category.append((category, page_obj))
+
+        context['menu_items_by_category'] = menu_items_by_category
+        return context
