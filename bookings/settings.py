807397529652256584912bf5efce1560c382cec0
Florin Pinta <xttrust@live.com>
2024-07-21 16:34:41 +0000

    Adder cloudinary to host images on separate server

--- bookings/settings.py
+++ bookings/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".codeinstitute-ide.net", ".herokuapp.com"]
 
@@ -46,11 +46,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'django_summernote',
+    'cloudinary',
     'pages',
     'table',
     'menu',
--- menu/models.py
+++ menu/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django_summernote.fields import SummernoteTextField
+from cloudinary.models import CloudinaryField
 
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100)
@@ -12,7 +13,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    # image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+    image = CloudinaryField('image', default='placeholder')
     
     def __str__(self):
         return self.name
